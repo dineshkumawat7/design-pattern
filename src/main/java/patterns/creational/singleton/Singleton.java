43147f5a2ea36935148fb5926a653df2262dcf88
Dinesh Kumawat <dkumawat7627@gmail.com>
2025-12-26 16:15:18 +0530

    Singleton design pattern

---
+++ src/main/java/patterns/creational/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package main.java.patterns.creational.singleton;
+
+/**
+ * Singleton pattern enables an application to create the one and only one instance
+ * of a Java class per JVM, in all possible scenarios.
+ */
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton(){
+        System.out.println("Singleton instance created!");
+    }
+
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
